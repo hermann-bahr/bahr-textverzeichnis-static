cf7acc90c06c6e06205b9da408bb7f074b4d6333
csae8092 <peter.andorfer@oeaw.ac.at>
2023-01-02 13:20:42 +0100

    copy listperson into data/indices

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,7 +1,10 @@
 rm -rf data/editions
+rm -rf data/indices
+mkdir data/indices
 mkdir data/editions
 wget https://github.com/hermann-bahr/bahr-textverzeichnis-data/archive/refs/heads/main.zip
 unzip main.zip && rm main.zip
+mv bahr-textverzeichnis-data-main/listperson.xml ./data/indices
 mv bahr-textverzeichnis-data-main/editions ./data
 rm -rf bahr-textverzeichnis-data-main
 ./dl_imprint.sh
