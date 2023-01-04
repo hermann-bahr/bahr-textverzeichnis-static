f5d687371a6ffa02400ae1946c208c9f2a1f81ec
Martin Anton Müller <martin.anton.mueller@oeaw.ac.at>
2023-01-04 16:43:32 +0100

    fetch data an neue struktur angepasst (?)

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,12 +1,6 @@
-rm -rf data/editions
-rm -rf data/indices
-rm -rf data/meta
-mkdir data/indices
-mkdir data/editions
-mkdir data/meta
+rm -rf data
 wget https://github.com/hermann-bahr/bahr-textverzeichnis-data/archive/refs/heads/main.zip
 unzip main.zip && rm main.zip
-mv bahr-textverzeichnis-data-main/listperson.xml ./data/indices
-mv bahr-textverzeichnis-data-main/editions ./data
+mv bahr-textverzeichnis-data-main/data ./data
 rm -rf bahr-textverzeichnis-data-main
 ./dl_imprint.sh
