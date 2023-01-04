3a28e0b855e31da4004e4ba43957dc028415f77a
Martin Anton Müller <martin.anton.mueller@oeaw.ac.at>
2023-01-04 16:32:18 +0100

    weiter

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,7 +1,9 @@
 rm -rf data/editions
 rm -rf data/indices
+rm -rf data/meta
 mkdir data/indices
 mkdir data/editions
+mkdir data/meta
 wget https://github.com/hermann-bahr/bahr-textverzeichnis-data/archive/refs/heads/main.zip
 unzip main.zip && rm main.zip
 mv bahr-textverzeichnis-data-main/listperson.xml ./data/indices
