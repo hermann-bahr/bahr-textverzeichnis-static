d0fdfdb38bb54688e18703b07f387db5e52862dc
Martin Anton Müller <martin.anton.mueller@oeaw.ac.at>
2023-01-04 12:15:49 +0100

    calendar muss auch generiert werden

--- process.sh
+++ process.sh
@@ -3,7 +3,7 @@ add-attributes -g "./data/meta/*.xml" -b "https://id.acdh.oeaw.ac.at.at/bahr-tex
 # add-attributes -g "./data/indices/*.xml" -b "https://id.acdh.oeaw.ac.at.at/bahr-textverzeichnis"
 
 # echo "make calendar data"
-# python make_calendar_data.py
+python make_calendar_data.py
 
 # echo "make typesense index"
 # python make_typesense_index.py
