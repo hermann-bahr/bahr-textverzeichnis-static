6d128fd410fb2ab303d68abcd285d4de9f9248b0
Martin Anton Müller <martin.anton.mueller@oeaw.ac.at>
2023-01-04 12:28:32 +0100

    nochmals kalender von pollaczek

--- make_calendar_data.py
+++ make_calendar_data.py
@@ -1,22 +1,33 @@
 import glob
 import os
 import json
+from datetime import datetime
 from acdh_tei_pyutils.tei import TeiReader
 from tqdm import tqdm
-files = glob.glob('./data/editions/*xml')
+
+os.makedirs('./html/js-data', exist_ok=True)
+files = sorted(glob.glob("./data/editions/*.xml"))
 out_file = "./html/js-data/calendarData.js"
 data = []
 for x in tqdm(files, total=len(files)):
-    item = {}
     head, tail = os.path.split(x)
     doc = TeiReader(x)
-    item['name'] = doc.any_xpath('//tei:titleStmt/tei:title[@level="a"]/text()')[0]
-    item['startDate'] = doc.any_xpath('//tei:titleStmt/tei:title[@type="iso-date"]/text()')[0]
-    item['id'] = tail.replace('.xml', '.html')
-    data.append(item)
+    counter = 0
+    for date in doc.any_xpath(".//tei:title[@when-iso]"):
+        item = {}
+        counter += 1
+        item["name"] = doc.any_xpath('//tei:title[@level="a"]/text()')[0]
+        date_str = f"{date.attrib['when-iso']}"
+        try:
+            datetime.fromisoformat(date_str)
+        except ValueError:
+            continue
+        item["startDate"] = date.attrib["when-iso"]
+        item["tageszaehler"] = counter
+        item["id"] = tail.replace(".xml", ".html")
+        data.append(item)
 
 print(f"writing calendar data to {out_file}")
-with open(out_file, 'w',  encoding='utf8') as f:
+with open(out_file, "w", encoding="utf8") as f:
     my_js_variable = f"var calendarData = {json.dumps(data, ensure_ascii=False)}"
     f.write(my_js_variable)
-
