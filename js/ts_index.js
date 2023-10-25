f858ea9a4381ece4f550924856f986f2ee498f1d
csae8092 <csae8092@users.noreply.github.com>
2023-10-25 11:00:33 +0000

    deploy: 2ca13edfd0d93e2f55cbcce1f49701855c16d7f0

--- js/ts_index.js
+++ js/ts_index.js
@@ -1,57 +1,57 @@
 const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
-    server: {
-      apiKey: "pPrlP1aGC6MtsyYcfuIzkaBOxxZsWjOg",
-      nodes: [
-        {
-          host: "typesense.acdh-dev.oeaw.ac.at",
-          port: "443",
-          protocol: "https",
-        },
-      ],
-      cacheSearchResultsForSeconds: 2 * 60,
-    },
-    additionalSearchParameters: {
-      query_by: "full_text"
-    },
-  });
+  server: {
+    apiKey: "scnRoJ47N3f66cPlrivxdDw44hKepDVv",
+    nodes: [
+      {
+        host: "typesense.acdh-dev.oeaw.ac.at",
+        port: "443",
+        protocol: "https",
+      },
+    ],
+    cacheSearchResultsForSeconds: 2 * 60,
+  },
+  additionalSearchParameters: {
+    query_by: "full_text"
+  },
+});
 
 
 const searchClient = typesenseInstantsearchAdapter.searchClient;
 const search = instantsearch({
-    indexName: 'bahr-textverzeichnis',
-    searchClient,
+  indexName: 'hbtv',
+  searchClient,
 });
 
 search.addWidgets([
-    instantsearch.widgets.searchBox({
-        container: '#searchbox',
-        autofocus: true,
-        cssClasses: {
-          form: 'form-inline',
-          input: 'form-control col-md-11',
-          submit: 'btn',
-          reset: 'btn'
-        },
-    }),
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+    autofocus: true,
+    cssClasses: {
+      form: 'form-inline',
+      input: 'form-control col-md-11',
+      submit: 'btn',
+      reset: 'btn'
+    },
+  }),
 
-    instantsearch.widgets.hits({
-        container: '#hits',
-        templates: {
-          empty: 'Keine Ergebnisse',
-          item: `
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      empty: 'Keine Ergebnisse',
+      item: `
               <h4><a href="{{ id }}.html">{{ title }}</a></h4>
               <p>{{#helpers.snippet}}{ "attribute": "full_text" }{{/helpers.snippet}}</p>
               <h5><span class="badge badge-primary">{{ project }}</span></h5>
               <div>
               </div>
           `
-      }
-    }),
+    }
+  }),
 
-    instantsearch.widgets.stats({
-      container: '#stats-container',
-      templates: {
-        text: `
+  instantsearch.widgets.stats({
+    container: '#stats-container',
+    templates: {
+      text: `
           {{#areHitsSorted}}
             {{#hasNoSortedResults}}Keine Treffer{{/hasNoSortedResults}}
             {{#hasOneSortedResults}}1 Treffer{{/hasOneSortedResults}}
@@ -65,45 +65,69 @@ search.addWidgets([
           {{/areHitsSorted}}
           gefunden in {{processingTimeMS}}ms
         `,
-      }
+    }
   }),
-    
-    instantsearch.widgets.pagination({
-        container: '#pagination',
-        padding: 2,
-        cssClasses: {
-          list: 'pagination',
-          item: 'page-item',
-          link: 'page-link'
-        }
-    }),
-    instantsearch.widgets.clearRefinements({
-        container: '#clear-refinements',
-        templates: {
-          resetLabel: 'Filter zurücksetzen',
-        },
-        cssClasses: {
-          button: 'btn'
-        }
-    }),
-
-    	
-
-    instantsearch.widgets.currentRefinements({
-      container: '#current-refinements',
-      cssClasses: {
-        delete: 'btn',
-        label: 'badge'
-      }
-    })
-]);
 
 
+  instantsearch.widgets.refinementList({
+    container: "#refinement-list-type",
+    attribute: "type",
+    searchable: true,
+    cssClasses: {
+        showMore: "btn btn-secondary btn-sm align-content-center",
+        list: "list-unstyled",
+        count: "badge m-2 badge-secondary hideme",
+        label: "d-flex align-items-center",
+        checkbox: "m-2",
+    },
+}),
+
+instantsearch.widgets.refinementList({
+  container: "#refinement-list-authors",
+  attribute: "authors.name",
+  searchable: true,
+  cssClasses: {
+      showMore: "btn btn-secondary btn-sm align-content-center",
+      list: "list-unstyled",
+      count: "badge m-2 badge-secondary hideme",
+      label: "d-flex align-items-center",
+      checkbox: "m-2",
+  },
+}),
+
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+    padding: 2,
+    cssClasses: {
+      list: 'pagination',
+      item: 'page-item',
+      link: 'page-link'
+    }
+  }),
+
+  instantsearch.widgets.clearRefinements({
+    container: '#clear-refinements',
+    templates: {
+      resetLabel: 'Filter zurücksetzen',
+    },
+    cssClasses: {
+      button: 'btn'
+    }
+  }),
+
+  instantsearch.widgets.currentRefinements({
+    container: '#current-refinements',
+    cssClasses: {
+      delete: 'btn',
+      label: 'badge'
+    }
+  })
+])
 
 search.addWidgets([
-    instantsearch.widgets.configure({
-        attributesToSnippet: ['full_text'],
-    })
+  instantsearch.widgets.configure({
+    attributesToSnippet: ['full_text'],
+  })
 ]);
 
 
