0d0aaf18f0c149ac80a7ab3e0850156daa511dc4
Martin Anton Müller <martin.anton.mueller@oeaw.ac.at>
2023-11-22 22:28:57 +0100

    tiny line fine nein

--- process.sh
+++ process.sh
@@ -1,4 +1,4 @@
-add-attributes -g "./data/editions/*.xml" -b "https://id.acdh.oeaw.ac.at.at/bahr-textverzeichnis"
+# add-attributes -g "./data/editions/*.xml" -b "https://id.acdh.oeaw.ac.at.at/bahr-textverzeichnis"
 add-attributes -g "./data/meta/*.xml" -b "https://id.acdh.oeaw.ac.at.at/bahr-textverzeichnis"
 # add-attributes -g "./data/indices/*.xml" -b "https://id.acdh.oeaw.ac.at.at/bahr-textverzeichnis"
 
