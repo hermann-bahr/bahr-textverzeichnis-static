9cd1952497b5d35c10a38f73e09e90c775efff54
martinantonmueller <martin.anton.mueller@oeaw.ac.at>
2025-11-17 09:51:36 +0100

    typesense retour

--- make_typesense_index.py
+++ make_typesense_index.py
@@ -5,37 +5,19 @@ from typesense.api_call import ObjectNotFound
 from acdh_cfts_pyutils import TYPESENSE_CLIENT as client
 from acdh_cfts_pyutils import CFTS_COLLECTION
 from acdh_tei_pyutils.tei import TeiReader
-from acdh_tei_pyutils.utils import (
-    extract_fulltext,
-    get_xmlid,
-    make_entity_label,
-)
 from tqdm import tqdm
 
 
 files = glob.glob("./data/editions/*.xml")
 
-COLLECTION_NAME = "hbtv"
 
-# Delete existing collection
 try:
-    result = client.collections[COLLECTION_NAME].delete()
-    print(f"Deleted existing collection '{COLLECTION_NAME}'")
+    client.collections["hbtv"].delete()
 except ObjectNotFound:
-    print(f"Collection '{COLLECTION_NAME}' does not exist yet, creating new one")
-except Exception as e:
-    print(f"Error deleting collection: {e}")
-    # Try to force delete
-    import time
-    time.sleep(2)
-    try:
-        client.collections[COLLECTION_NAME].delete()
-        print(f"Retry: Successfully deleted '{COLLECTION_NAME}'")
-    except:
-        pass
+    pass
 
 current_schema = {
-    "name": COLLECTION_NAME,
+    "name": "hbtv",
     "enable_nested_fields": True,
     "fields": [
         {"name": "id", "type": "string"},
@@ -50,37 +32,10 @@ current_schema = {
             "optional": True,
             "facet": True,
         },
-        {
-            "name": "persons",
-            "type": "object[]",
-            "facet": True,
-            "optional": True,
-        },
-        {
-            "name": "places",
-            "type": "object[]",
-            "facet": True,
-            "optional": True,
-        },
-        {
-            "name": "orgs",
-            "type": "object[]",
-            "facet": True,
-            "optional": True,
-        },
-        {
-            "name": "works",
-            "type": "object[]",
-            "facet": True,
-            "optional": True,
-        },
     ],
 }
 
-# Create new collection with updated schema
-created = client.collections.create(current_schema)
-print(f"Created collection 'hbtv' with schema: {list(current_schema['fields'][0].keys())}")
-print(f"Total fields in schema: {len(current_schema['fields'])}")
+client.collections.create(current_schema)
 
 records = []
 cfts_records = []
@@ -129,63 +84,15 @@ for x in tqdm(files, total=len(files)):
         cfts_record["year"] = int(date_str[:4])
     except ValueError:
         pass
-
-    # Extract entities from back section
-    record["persons"] = []
-    for y in doc.any_xpath(".//tei:back//tei:person[@xml:id]"):
-        try:
-            entity_id = get_xmlid(y)
-            label_result = make_entity_label(y.xpath("./*[1]")[0])
-            if label_result and len(label_result) > 0:
-                item = {"id": entity_id, "label": label_result[0]}
-                record["persons"].append(item)
-        except (IndexError, AttributeError, TypeError) as e:
-            pass
-    cfts_record["persons"] = [x["label"] for x in record["persons"]]
-
-    record["places"] = []
-    for y in doc.any_xpath(".//tei:back//tei:place[@xml:id]"):
-        try:
-            entity_id = get_xmlid(y)
-            label_result = make_entity_label(y.xpath("./*[1]")[0])
-            if label_result and len(label_result) > 0:
-                item = {"id": entity_id, "label": label_result[0]}
-                record["places"].append(item)
-        except (IndexError, AttributeError, TypeError) as e:
-            pass
-    cfts_record["places"] = [x["label"] for x in record["places"]]
-
-    record["orgs"] = []
-    for y in doc.any_xpath(".//tei:back//tei:org[@xml:id]"):
-        try:
-            entity_id = get_xmlid(y)
-            label_result = make_entity_label(y.xpath("./*[1]")[0])
-            if label_result and len(label_result) > 0:
-                item = {"id": entity_id, "label": label_result[0]}
-                record["orgs"].append(item)
-        except (IndexError, AttributeError, TypeError) as e:
-            pass
-
-    record["works"] = []
-    for y in doc.any_xpath(".//tei:back//tei:bibl[@xml:id]"):
-        try:
-            entity_id = get_xmlid(y)
-            label_result = make_entity_label(y.xpath("./*[1]")[0])
-            if label_result and len(label_result) > 0:
-                item = {"id": entity_id, "label": label_result[0]}
-                record["works"].append(item)
-        except (IndexError, AttributeError, TypeError) as e:
-            pass
-
     record["full_text"] = " ".join("".join(body.itertext()).split())
     cfts_record["full_text"] = record["full_text"]
     records.append(record)
     cfts_records.append(cfts_record)
 
-make_index = client.collections[COLLECTION_NAME].documents.import_(records)
+make_index = client.collections["hbtv"].documents.import_(records)
 print(make_index)
-print(f"done with indexing {COLLECTION_NAME}")
+print("done with indexing hbtv")
 
 make_index = CFTS_COLLECTION.documents.import_(cfts_records, {"action": "upsert"})
 print(make_index)
-print(f"done with cfts-index {COLLECTION_NAME}")
+print("done with cfts-index hbtv")
