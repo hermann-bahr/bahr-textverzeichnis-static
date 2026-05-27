038a7f50b9b1e9b2e2b4125543c5b191752627a8
martinantonmueller <martin.anton.mueller@oeaw.ac.at>
2026-05-27 16:53:33 +0200

    nochmals

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,5 +1,5 @@
-rm -rf data
-wget https://github.com/hermann-bahr/bahr-textverzeichnis-data/archive/refs/heads/main.zip
+rm -rf data main.zip
+wget -O main.zip https://github.com/hermann-bahr/bahr-textverzeichnis-data/archive/refs/heads/main.zip
 unzip main.zip && rm main.zip
 mv bahr-textverzeichnis-data-main/data ./data
 rm -rf bahr-textverzeichnis-data-main
